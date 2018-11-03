9a1ca7db13b1d53c5de2f7ea235caaa5b0864d2f
riley serino <rileyserino@gmail.com>
2018-11-03 12:04:45 -0400

    Added Game class and hit function

--- blackjack.py
+++ blackjack.py
@@ -1,5 +1,6 @@
 import random
 
+
 class Hand(object):
     def __init__(self, hand):
         self.hand = hand
@@ -11,6 +12,7 @@ class Hand(object):
         self.hand.append(new_card)
 
     def print_hand(self):
+        print("\n")
         for card in self.hand:
             card_suit, card_value = card.get_card()
             print(card_value, card_suit, ' ', end = '', sep = '')
@@ -67,6 +69,46 @@ class Card(object):
     def get_card(self):
         return self.suit, self.value
 
+class Game(object):
+    def __init__(self):
+        self.game = self.start_game()
+
+
+    def start_game(self):
+        deck = Deck()
+        hand = deck.deal()
+        hand.print_hand()
+        score = hand.calc_score()
+        Game.post_deal(score)
+
+
+
+    def post_deal(score):
+        if score == 21:
+            while True:
+                print("Blackjack! Would you like to play again?")
+                again_response = input("(y/n) > ")
+                if again_response in ['y', 'Y']:
+                    game = Game()
+                    return game
+                elif again_response in ['n', 'N']:
+                    print("Ok bye!")
+                    exit()
+                else:
+                    ("You must enter either a 'Y' or an 'N'.")
+        if score < 21:
+            while True:
+                print("Would you like to hit or stay?")
+                hit_stay_response = input("h/s > ")
+                if hit_stay_response in ['h', 'H']:
+                    pass
+                elif hit_stay_response in ['s', 'S']:
+                    pass
+                else:
+                    print("You must enter either an 'H' or a 'S'.")
+
+
+
     # def print_card(self):
     #     for card in self:
     #         card_suit, card_value = card.get_card()
@@ -75,15 +117,15 @@ class Card(object):
 
 def main():
 
-    deck = Deck()
-    hand = deck.deal()
-    hand.print_hand()
-    hand.calc_score()
-    new_card = deck.hit()
-    hand.append_hit(new_card)
-    hand.print_hand()
-    hand.calc_score()
-    print(len(deck.get_deck()))
+    game = Game()
+    # deck = Deck()
+    # hand = deck.deal()
+    # hand.print_hand()
+    # hand.calc_score()
+    # hand.append_hit(deck.hit())
+    # hand.print_hand()
+    # hand.calc_score()
+    # print(len(deck.get_deck()))
 
 
 main()
