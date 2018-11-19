2b1767611d70488506476e9da667397aed8d078f
riley serino <rileyserino@gmail.com>
2018-11-18 21:41:16 -0500

    Add hidden dealer card

--- Game.py
+++ Game.py
@@ -1,6 +1,7 @@
 from Deck import Deck
 from Hand import Hand
 
+
 class Game(object):
     def __init__(self):
         self.deck = Deck()
@@ -10,28 +11,36 @@ class Game(object):
 
     def start(self):
         again = True
+        player_win = 0
+        hit_stay_response = 'h'
 
         while again:
-            print("\nDealer hand:")
-            self.dealer.print_hand()
-            dealer_score = self.dealer.calc_score()
+            if hit_stay_response.lower() == 'h':
+                print("\nDealer hand:")
+                self.dealer.print_dealer_hand()
+                # self.dealer.print_hand()
+                dealer_score = self.dealer.calc_score()
+            else:
+                print("\nDealer hand:")
+                self.dealer.print_hand()
+                # self.dealer.print_hand()
+                dealer_score = self.dealer.calc_score()
 
             print("\n\nPlayer hand:")
             self.player.print_hand()
-            player_score = self.player.calc_score()
+            player_score = self.player.print_score()
 
             if player_score == 21 and len(self.player.get_hand()) == 2:
                 print("Blackjack!")
-                player_win = True
+                player_win = 1
                 again = False
 
             if player_score == 21 and len(self.player.get_hand()) > 2 and dealer_score < 18:
-                self.dealer_turn(dealer_score, player_score)
+                player_win = self.dealer_turn(dealer_score, player_score)
                 again = False
 
             if player_score == 21 and len(self.player.get_hand()) > 2 and dealer_score > 18 and dealer_score < 21:
-                # print("You win!")
-                player_win = True
+                player_win = 1
                 again = False
 
             if player_score < 21:
@@ -50,42 +59,50 @@ class Game(object):
 
             if player_score > 21:
                 print("\nBust!")
-                player_win = False
+                player_win = 2
                 again = False
 
-        if player_win == True:
+        if player_win == 1:
             print("\nYou win!")
-        if player_win == False:
+        if player_win == 2:
             print("\nYou lose!")
+        if player_win == 3:
+            print("\nPush!")
         print("Bye!\n")
 
     def dealer_turn(self, dealer_score, player_score):
-        player_win = False
+        player_win = 0
         if dealer_score < 17 and dealer_score <= player_score:
+            print("\nDealer Hits.")
             self.dealer.append_hit(self.deck.hit())
             print("\nDealer hand:")
             self.dealer.print_hand()
-            self.dealer_turn(self.dealer.calc_score(), player_score)
+            return self.dealer_turn(self.dealer.print_score(), player_score)
 
         if dealer_score >=17 and dealer_score < player_score:
-            player_win = True
-            # print("\nYou win!")
+            player_win = 1
+            return player_win
 
         if dealer_score >= 17 and dealer_score < 21 and dealer_score > player_score:
-            player_win = False
-            # print("\nDealer wins!")
+            player_win = 2
+            return player_win
 
         if dealer_score == player_score:
-            player_win = False
-            print("\nPush!")
+            player_win = 3
+            return player_win
 
         if dealer_score < 21 and dealer_score > player_score:
-            player_win = False
-            # print("\nDealer wins!")
+            player_win = 2
+            return player_win
 
         if dealer_score > 21:
             print("\nDealer Bust!")
-            player_win = True
+            player_win = 1
+            return player_win
+
+        if dealer_score == 21 and dealer_score > player_score:
+            player_win = 2
+            return player_win
 
         return player_win
 
