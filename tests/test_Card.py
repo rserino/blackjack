626654741e8917702847c68001bf499e045fa00b
Jonas Oppenheim <oppenheim10@gmail.com>
2018-12-31 21:11:34 -0500

    Restructure project directory

---
+++ tests/test_Card.py
@@ -0,0 +1,17 @@
+import unittest
+from blackjack.Card import Card
+
+class TestCardClass(unittest.TestCase):
+  def setUp(self):
+    self.card = Card('♥', 5)
+
+  def test_get_card(self):
+    '''get_card() returns correct tuple'''
+    self.assertEqual(self.card.get_card(), ('♥', 5))
+
+  def test_to_string(self):
+    '''to_string() returns correct string'''
+    self.assertEqual(self.card.to_string(), '5♥ ')
+
+if __name__ == '__main__':
+  unittest.main()
