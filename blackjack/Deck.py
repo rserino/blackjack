55efdb93b5fe84e3d7612d2fe2df84350c060e5c
Jonas Oppenheim <oppenheim10@gmail.com>
2019-01-07 21:06:28 -0500

    Add pull_card() method to Deck with tests

--- blackjack/Deck.py
+++ blackjack/Deck.py
@@ -1,32 +1,36 @@
-from Card import Card
-from Hand import Hand
+from .Card import Card
+from .Hand import Hand
 import random
 
 class Deck(object):
-    def __init__(self):
-        self.deck = self.create_deck()
+  def __init__(self):
+    self.deck = self.create_deck()
 
-    def get_deck(self):
-        return self.deck
+  def get_deck(self):
+    return self.deck
 
-    def create_deck(self):
-        deck = []
-        suits = ['♥', '♦', '♣', '♠']
-        values = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A']
+  def create_deck(self):
+    deck = []
+    suits = ['♥', '♦', '♣', '♠']
+    values = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A']
 
-        for suit in suits:
-            for value in values:
-                deck.append(Card(value, suit))
+    for suit in suits:
+      for value in values:
+        deck.append(Card(value, suit))
 
-        return deck
+    return deck
 
-    def deal(self):
-        random.shuffle(self.deck)
+  def deal(self):
+    random.shuffle(self.deck)
 
-        card_one = self.deck.pop(0)
-        card_two = self.deck.pop(0)
+    card_one = self.deck.pop(0)
+    card_two = self.deck.pop(0)
 
-        return Hand([card_one, card_two])
+    return Hand([card_one, card_two])
 
-    def hit(self):
-        return self.deck.pop(0)
+  def hit(self):
+    return self.deck.pop(0)
+  
+  def pull_card(self, value, suit):
+    card = [card for card in self.deck if card.value == value and card.suit == suit][0]
+    return self.deck.pop(self.deck.index(card))
